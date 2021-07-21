94fac1cf7fbd52a092fd08043bebd7b8f0b794b7
seulda <kim02977@naver.com>
2021-07-21 17:19:00 +0900

    test setting

---
+++ src/main/java/org/corona/mapper/TestMapper.java
@@ -0,0 +1,5 @@
+package org.corona.mapper;
+
+public class TestMapper {
+	//임시파일입니다. 맵퍼 사용시 새파일을 만들어 사용하세요.
+}
---
+++ src/main/java/org/corona/service/TestService.java
@@ -0,0 +1,5 @@
+package org.corona.service;
+
+public interface TestService {
+	//임시파일입니다. Service 사용시 새파일을 만들어 사용하세요.
+}
