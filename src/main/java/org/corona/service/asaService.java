e1e595f172f68dcf4d1f012294d9b94d69eda8cf
wns1908 <williueam@gmail.com>
2021-07-22 13:17:57 +0900

    asa

--- src/main/java/org/corona/service/asaService.java
+++ src/main/java/org/corona/service/asaService.java
@@ -7,6 +7,4 @@ import org.corona.domain.ASAVO;
 public interface asaService {
 	
 	public ArrayList<ASAVO> asarea(String startCreateDt, String endCreateDt) throws Exception;
-	
-//	public ArrayList<ASAVO> asarea1(String startCreateDt, String endCreateDt) throws Exception;
 }
